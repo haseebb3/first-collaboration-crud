a3b3d1ce25c857837e4a40eed163850512fd2362
chaudharivaishu90-debug <chaudharivaishu90@gmail.com>
2026-09-25 12:09:50 +0530

    added my features

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,11 +6,83 @@ const contact = document.getElementById("contact");
 const addStudentBtn = document.getElementById("addStudentBtn");
 const updateStudentBtn = document.getElementById("updateStudentBtn");
 const studentsContainer = document.getElementById("studentsContainer");
+const spinner = document.getElementById("spinner");
+
 
 
 
 const baseUrl = "https://posts-crud-c2796-default-rtdb.firebaseio.com";
 const studentsUrl = `${baseUrl}/students.json`;
+
+
+let studentArr=[];
+
+function hideSpinner(){
+    spinner.classList.add("d-none")
+
+}
+function showSpinner(){
+    spinner.classList.remove("d-none")
+
+}
+
+function OnCreatestd(eve){
+    eve.preventDefault();
+    let obj={
+        fname:fname.value,
+        lname:lname.value,
+        email:email.value,
+        contact:contact.value
+    }
+        showSpinner();
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST",studentsUrl);
+    xhr.setRequestHeader("content-type","application/json");
+    xhr.setRequestHeader("TOKEN","JWT FROM LS");
+    xhr.send(JSON.stringify(obj));
+    xhr.onload=function(){
+        if(xhr.status === 200 && xhr.status <=299){
+            let res=JSON.parse(xhr.response);
+            hideSpinner();
+            let tr=document.createElement("tr");
+            tr.id=res.name;
+            tr.innerHTML=`<td>${1}</td>
+                            <td>${obj.fname}</td>
+                            <td>${obj.lname}</td>
+                            <td>${obj.email}</td>
+                            <td>${obj.contact}</td>
+                            
+                      <td><button onclick="onEdit(this)" class="btn btn-outline-info btn-sm">Edit</button></td>
+                      <td><button onclick="onDelete(this)" class="btn btn-outline-danger btn-sm">Delete</button></td>`
+                            studentsContainer.prepend(tr);
+                            studentForm.reset();
+
+                            let td=[...document.querySelectorAll("#studentsContainer tr td:first-child")];
+                             td.forEach((td,i) => {
+                                td.innerHTML=i+1
+                            });
+                            Swal.fire({
+                title:"list  Created  successfully",
+                timer:"2000",
+                icon:"success"
+            });
+            }else{
+                    cl("error")
+                }
+    }
+}
+
+
+studentForm.addEventListener("submit",OnCreatestd)
+
+
+
+
+
+
+
+
+
 
 
 function onDelete(ele){
